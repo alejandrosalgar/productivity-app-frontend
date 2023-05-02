ceb6ed2034629d1b252cfc8382c92ab63997e668
Alejandro Salgar Marín <alejandro.salgar@gmail.com>
2023-05-01 20:51:14 -0500

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -43,7 +43,7 @@ const Login = () => {
           <label htmlFor="password">Password:</label>
           <input type="password" id="password" name="password" />
         </span>
-        <button type="submit">Logiasdfgfdgsdfn</button>
+        <button type="submit">Login</button>
       </form>
     </div>
   );
