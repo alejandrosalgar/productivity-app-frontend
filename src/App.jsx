48bf278db04ee13961f65897e524495796b6ba73
Alejandro Salgar Marín <alejandro.salgar@gmail.com>
2023-05-03 18:37:41 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -77,7 +77,7 @@ const App = () => {
             />
           </div>
           <button type="submit" data-testid="activity-add-button">
-            Add
+            Add Activity
           </button>
         </form>
       </header>
