8a6500c4115d02cbdc072f9b240d4d236c9cae59
alejandrosalgar <alejandro.salgar@gmail.com>
2023-05-12 17:14:11 -0500

    lmasdlmasld

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const token = localStorage?.getItem("token");
 
 root.render(<React.StrictMode>{token ? <App /> : <Login />}</React.StrictMode>);
 
-// If you want to start measuring performance in your app, pass a function
+// If you want to start measuring performance in your
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
